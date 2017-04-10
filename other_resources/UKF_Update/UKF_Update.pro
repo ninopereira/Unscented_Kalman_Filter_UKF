0975fff9eb71c040c0958649dfcfefda786a462e
Nino Pereira <nino.pereira@dyson.com>
2017-04-10 16:47:42 +0100

    all lesson's projects finished

---
+++ other_resources/UKF_Update/UKF_Update.pro
@@ -0,0 +1,22 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2017-04-10T14:18:15
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = UKF_Update
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp \
+    ukf.cpp
+
+HEADERS += \
+    ukf.h
---
+++ other_resources/build-Sigma_Point_Prediction-gcc-Default/CMakeFiles/Sigma_Point_Prediction.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Sigma_Point_Prediction.dir/main.cpp.o"
+  "CMakeFiles/Sigma_Point_Prediction.dir/ukf.cpp.o"
+  "Sigma_Point_Prediction.pdb"
+  "Sigma_Point_Prediction"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Sigma_Point_Prediction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
